f735efbba5a868434e5f6fcda78cc79fa7f3e13c
aliasgis <aliasgis@gmail.com>
2019-10-20 23:11:21 +0900

    2주차

--- feature.py
+++ feature.py
@@ -2,7 +2,8 @@ import osgeo.ogr
 shapefile = osgeo.ogr.Open("GP_ETC.shp")
 layer = shapefile.GetLayer(0)
 for i in range(layer.GetFeatureCount()):
-feature =layer.GetFeature(i)
-geometry = feature.GetGeometryRef()
-geometry_type = geometry.GetGeometryName(()
-print 'geometry ({0},{1}'.format(i, geometry_type)
+	feature =layer.GetFeature(i)
+	geometry = feature.GetGeometryRef()
+	geometry_type = geometry.GetGeometryName()
+	feature_Name = feature.getField("id")
+print 'geometry ({0},{1},{2})'.format(i, geometry_type, feature_Name)
---
+++ patch.py
@@ -0,0 +1,3 @@
+from shapely.geometry import Point
+ patch = Point(0.0, 0.0).buffer(10.0)
+ patch
